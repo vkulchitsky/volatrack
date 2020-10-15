be9ed7a7c77e12eca858e620aa161a65b429ef6e
Vladimir A Kulchitsky <vladimir.kulchitsky@coupi.us>
2020-10-15 13:25:01 +0300

    added defs again

--- defs.hpp
+++ defs.hpp
@@ -1,3 +1,45 @@
+/*!
+  Definitions for json strings
+  @file defs.hpp
+  @author Vladimir A Kulchitsky
+  @version 1.0 10/15/2020
+*/
+
+#define MAT_RHO "rho"
+#define MAT_G "G"
+#define MAT_NU "nu"
+#define MAT_GAMMA "gamma"
+#define MAT_CR0 "CR0"
+#define MAT_VRO "vR0"
+#define MAT_ALPHA "alpha_t"
+#define MAT_MU "mu"
+#define MAT_RMU "rMu"
+#define MAT_RNU "rNu"
+#define MAT_RK "rK"
+#define MAT_RTH "rTh"
+#define MAT_VISC "visc"
+#define MAT_VISCG "viscg"
+#define MAT_NAME_F "name"
+
+#define SPHERE_POSITION "position"
+#define SPHERE_ROTATION "orientation"
+#define SPHERE_VELOCITY "velocity"
+#define SPHERE_RADIUS "radius"
+#define SPHERE_TEMPERATURE "temperature"
+#define SPHERE_MATERIAL "rho"
+
+#define TIME_CURRENT "current time"
+#define TIME_STEP "time step"
+#define TIME_SAVE_STEP "save time step"
+
+#define VOL_SPH_INDEX "isphere"
+#define VOL_SURLOC "surface location"
+#define VOL_FLAGS "flags"
+
+#define DATA_SPHERES "spheres"
+#define DATA_VOLS "volatiles"
+#define DATA_TIME "time"
+
 #ifndef DEFS_HPP
 #define DEFS_HPP
 
